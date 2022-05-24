589b5f80606d6784566cfa5fdc6760ff995a53aa
Alejandro Villarino <alejandro.villarino@correo.unimet.edu.ve>
2022-05-24 17:03:28 -0400

    interfaz pt1

--- Proyecto 1 SO - JS & AV/src/Main.java
+++ Proyecto 1 SO - JS & AV/src/Main.java
@@ -1,3 +1,6 @@
+
+import interfaz.maininterfaz;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -10,6 +13,6 @@
  */
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        maininterfaz menu = new maininterfaz();
     }
 }
